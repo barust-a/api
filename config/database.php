968719512416395401b24cd5ea8fa6f11b3b750b
barust-a <barust_a@etna-alternance.net>
2018-03-23 15:11:12 +0100

    added keys app + modified gitignore

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'restos'),
-            'username' => env('DB_USERNAME', 'phpmyadmin'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
