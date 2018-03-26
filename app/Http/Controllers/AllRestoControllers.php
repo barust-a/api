cba62013d75157a83a71e7b47f26ec8d79c0238b
pauyo <gailha_p@etna-alternance.com>
2018-03-26 18:55:00 +0200

    PUTAIN DE PROJET D'ENCULÉ DE TA GROSSE MÈRE DE FILS DE PUTE DE TA RACE

--- app/Http/Controllers/AllRestoControllers.php
+++ app/Http/Controllers/AllRestoControllers.php
@@ -9,9 +9,12 @@
 namespace App\Http\Controllers;
 use Couchbase\Document;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class AllRestoControllers extends Controller
 {
+    private $successStatus = 200;
+
     function GetAllResto()
     {
             $allresto = DB::table('restaurants')->get();
@@ -34,5 +37,20 @@ class AllRestoControllers extends Controller
     {
         DB::table('restaurants')->where('id', $id)->delete();
         return response()->json(['success'=> 'deleted'], $this->successStatus);
+    }
+
+    function updateResto(Request $request, $id)
+    {
+        $all = $request->all();
+        return response()->json($all);
+        /*$table = [
+            'nom' => $all['nom'],
+            'email' => $all['email'],
+            'pays' => $all['pays'],
+            'ville' => $all['ville'],
+            'code_postal' => ['code_postal']
+        ];
+        $article = DB::table('restaurants')->where('id', $id)->update($table);
+        return response()->json(['success'=> $article], $this->successStatus);*/
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,51 @@ Route::group(['middleware' => 'auth:api'], function(){
     Route::post('get-details', 'API\PassportController@getDetails');
 
 });
+
+//User Route
+
+Route::get('/users', 'AllUserControler@GetAllUsers');
+
+Route::get('/users/{id}', 'AllUserControler@GetUserId')->where('name', '[0-9]+');
+
+Route::delete('/delete-user/{$id}', 'AllUserControler@DeleteUser');
+
+Route::get('/userName/{name}', 'AllUserControler@GetUserName');
+
+//Resto Route
+
+Route::get('/restos', 'AllRestoControllers@GetAllResto');
+
+Route::get('/restos/{id}', 'AllRestoControllers@GetRestoId')->where('name', '[0-9]+');
+
+Route::delete('/delete-resto/{id}', 'AllRestoControllers@DeleteResto');
+
+Route::get('/restoName/{name}', 'AllRestoControllers@GetRestoName');
+
+Route::post('/uprestos/{id}','AllRestoControllers@updateResto');
+
+//menu route
+
+Route::get('/menus', 'MenuController@GetAllMenu');
+
+Route::get('/menus/{id}', 'MenuController@GetMenu')->where('name', '[0-9]+');
+
+Route::delete('/delete-menu/{id}', 'MenuController@DeleteMenu');
+
+Route::get('/menuResto/{id}', 'MenuController@GetRestoMenu');
+
+
+//Avis Route
+
+Route::get('/avis', 'AvisController@GetAllAvis');
+
+Route::get('/avis/{id}', 'AvisController@GetAvis')->where('name', '[0-9]+');
+
+Route::delete('/delete-avis/{id}', 'AvisController@DeleteAvis');
+
+Route::get('/avisResto/{id}', 'AvisController@GetRestoAvis');
+
+Route::get('/avisUser/{id}', 'AvisController@GetUserAvis');
+
+Route::get('/avisUserResto/{id_user}/{id_resto}', 'AvisController@GetRestoUserAvis');
+
